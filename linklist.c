c7acccd386ac09743d9aaaff3267c1a84e967f7e
lixing0415 <lixing0415@163.com>
2014-09-23 23:42:16 +0800

    Create linklist.c

---
+++ linklist.c
@@ -0,0 +1,35 @@
+/**************************************************************************************************/
+/* Copyright (C) mc2lab.com, SSE@USTC, 2014-2015                                                  */
+/*                                                                                                */
+/*  FILE NAME             :  linklist.c                                                           */
+/*  PRINCIPAL AUTHOR      :  Lixing (Come from Mengning's teaching videoes)                       */
+/*  SUBSYSTEM NAME        :  linklist                                                             */
+/*  MODULE NAME           :  linklist function                                                    */
+/*  LANGUAGE              :  C                                                                    */
+/*  TARGET ENVIRONMENT    :  ANY                                                                  */
+/*  DATE OF FIRST RELEASE :  2014/09/22                                                           */
+/*  DESCRIPTION           :  This is a linklist source program                                    */
+/**************************************************************************************************/
+
+/*
+ * Revision log:
+ *
+ * Writed by Lixing, after studying Mengning's teaching videoes,2014/09/22
+ *
+ */
+
+/* data struct and its operations */
+
+typedef struct CmdNode
+{
+    char*   cmd;
+    char*   desc;
+    int     (*handler)();
+    struct  CmdNode *next;
+}tCmdNode;
+
+/* find a cmd in the linklist and return the cmdnode pointer */
+tCmdNode* FindCmd(tCmdNode * head, char * cmd);
+
+/* show all cmd in listlist */
+int ShowAllCmd(tCmdNode * head);
